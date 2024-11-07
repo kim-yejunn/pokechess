90a973d5e9a6a780f43d1327f2f5cdcbf93c2504
김예준 <72857855+kim-yejunn@users.noreply.github.com>
2024-11-07 10:52:47 +0900

    이미지

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from 'react';
 import ChessBoard from '../components/ChessBoard'; // Adjust path as needed
 import socket from '../utils/socket.js'; // Adjust path as needed
 
+// 체스 기물 이미지 URL 변수 정의
+const bK = 'path/to/black_knight.png';
+const bT = 'path/to/black_bishop.png';
+const wK = 'path/to/black_queen.png';
+const wT = 'path/to/black_king.png';
+
 const initialChessboardState = [
-    ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
-    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
-    ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
+    ['', 'bt', 'bk', 'bk', 'bt', ''],
+    ['', '', 'bk', 'bk', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', 'wk', 'wk', '', ''],
+    ['', 'wk', 'wt', 'wt', 'wk', '']
 ];
 
 const GamePage = () => {
