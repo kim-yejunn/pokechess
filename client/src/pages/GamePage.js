0e23b12708ee402883920adc51e9942c77a862ca
김예준 <72857855+kim-yejunn@users.noreply.github.com>
2024-11-07 10:53:32 +0900

    경로 수정

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -3,10 +3,10 @@ import ChessBoard from '../components/ChessBoard'; // Adjust path as needed
 import socket from '../utils/socket.js'; // Adjust path as needed
 
 // 체스 기물 이미지 URL 변수 정의
-const bK = 'path/to/black_knight.png';
-const bT = 'path/to/black_bishop.png';
-const wK = 'path/to/black_queen.png';
-const wT = 'path/to/black_king.png';
+const bK = '/Users/dust/Documents/GitHub/pokechess/client/public/pic_img/bK.png';
+const bT = '/Users/dust/Documents/GitHub/pokechess/client/public/pic_img/bT.png';
+const wK = '/Users/dust/Documents/GitHub/pokechess/client/public/pic_img/wK.png';
+const wT = '/Users/dust/Documents/GitHub/pokechess/client/public/pic_img/wT.png';
 
 const initialChessboardState = [
     ['', 'bt', 'bk', 'bk', 'bt', ''],
